3503d1e2103c811a435bca80c5b2d87fd9e237ad
Swapnil Jaiswal <swapnil.jaiswal@tradus.com>
2016-06-21 14:48:51 +0530

    updated the script to correctly fetch the image name & snapshot names

--- ami-automate/ami-create_custom.sh
+++ ami-automate/ami-create_custom.sh
@@ -1,4 +1,60 @@
 #!/bin/bash
+#Script to create AMI of server on daily basis and deleting AMI older than n (= 7) no of days
+## Location of this bash file should be in /custom_scripts/
+mkdir -p /custom_scripts/
+cd /custom_scripts/
+## in case you don't want to take instance id from an external text file you can hardcode as below##
+#declare -a ami_ids=('i-ami1' 'i-ami2' 'i-ami3' );
+ami_ids='/custom_scripts/ami.txt'
+
+for i in `cat $ami_ids`
+do
+
+echo -e "----------------------------------\n Picking up instance $i at `date`   \n----------------------------------"
+
+##To create a unique AMI name for this script
+echo "$i-`date +%d%b%y`" > /tmp/$i\_aminame.txt
+
+
+echo -e "Starting the Daily AMI creation for  instance: `cat /tmp/$i\_aminame.txt`\n"
+
+#To create AMI of defined instance
+aws ec2 create-image --instance-id $i --name "`cat /tmp/$i\_aminame.txt`"  --description "Automated AMI for \$i\ " --no-reboot >  /tmp/$i\_amiID.txt
+
+##Showing the AMI name created by AWS
+echo -e "AMI ID is: `cat  /tmp/$i\_amiID.txt `\n"
+
+##Finding AMI older than 1 day which needed to be removed
+echo -e "Looking for AMI older than 1 days:\n "
+echo "$i-`date +%d%b%y --date '1 days ago'`" > /tmp/$i\_amidel.txt
+
+##Finding Image ID of instance which needed to be Deregistered
+aws ec2 describe-images --filters "Name=name,Values=`cat /tmp/$i\_amidel.txt`" | grep -i ami | awk '{ print  $9 }' > /tmp/$i\_imageid.txt
+
+if [[ -s /tmp/$i\_imageid.txt ]];
+then
+
+echo -e "Following AMI is found : `cat /tmp/$i\_imageid.txt`\n"
+#
+##Find the snapshots attached to the Image need to be Deregister
+aws ec2 describe-images --image-ids `cat /tmp/$i\_imageid.txt` | grep snap | awk ' { print $4 }' > /tmp/$i\_snap.txt
+
+echo -e "Following are the snapshots associated with it :\n`cat /tmp/$i\_snap.txt`\n"
+echo -e "Starting the Deregister of AMI... \n"
+
+#Deregistering the AMI
+aws ec2 deregister-image --image-id `cat /tmp/$i\_imageid.txt`
+#
+###Deleting snapshots attached to AMI
+echo -e "\nDeleting the associated snapshots.... \n"
+for i in `cat /tmp/$i\_snap.txt`;do aws ec2 delete-snapshot --snapshot-id $i ; done
+
+else
+echo -e "No AMI found older than minimum required no of days"
+fi
+
+done
+#!/bin/bash
 #Script to create AMI of server on daily basis and deleting AMI older than n no of days
 
 ## in case you don't want to take instance id from an external text file you can hardcode as below##
